a458321f5a11c2922be855dc2b1659afbc91319c
EmersonMT <emerson.mejia.t@gmail.com>
2025-11-12 21:05:13 -0500

    Aplicar nuevo .gitignore y limpiar archivos ignorados

--- build/xarm_gui/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/emersonmt/topics_ws/install/xarm_gui'
--- build/xarm_sdk/CMakeFiles/xarm_cxx_sdk.dir/cxx/src/serial/impl/list_ports/list_ports_win.cc.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/xarm_cxx_sdk.dir/cxx/src/serial/impl/list_ports/list_ports_win.cc.o: \
- /home/emersonmt/topics_ws/src/xarm_ros2/xarm_sdk/cxx/src/serial/impl/list_ports/list_ports_win.cc \
- /usr/include/stdc-predef.h
